45c485c0bed6b90b2d209e8796b0c7fe75a87f94
raptor0929 <flaura.dev@gmail.com>
2025-08-31 00:06:52 -0400

    update bash scripts

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
 source .env
 
-forge script script/Deploy.s.sol:DeployScript --rpc-url https://lisk.drpc.org --broadcast --verify
+forge script script/Deploy.s.sol:DeployScript \
+ --etherscan-api-key $ETHERSCAN_API_KEY \
+ --rpc-url https://lisk.drpc.org \
+ --broadcast \
+ --verifier blockscout \
+ --verifier-url 'https://blockscout.lisk.com/api/' \
+ --verify \
+ -vvvv
